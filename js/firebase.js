a6627e21d51dd29dca11edeed5fd032c81f87649
JarAr01 <74222048+JarAr01@users.noreply.github.com>
2021-04-03 22:51:30 -0600

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBxc_n-79XRptAclnBA1Ec5-5dpLTitCYQ",
+    authDomain: "espjaret.firebaseapp.com",
+    projectId: "espjaret",
+    storageBucket: "espjaret.appspot.com",
+    messagingSenderId: "211119785674",
+    appId: "1:211119785674:web:f252c4189a81b7c7589633"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
